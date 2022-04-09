ffb7a7e0ed63c1ff1ff8c4e707beb18430823310
vhfuly <victor_hugo_fuly@hotmail.com>
2022-04-09 11:45:29 -0300

    feat: create console command

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccount } from './models/bank-account';
+import { BanckAccountController } from './controllers/banck-account/banck-account.controller';
+import { ConsoleModule } from 'nestjs-console';
+import { FixturesCommand } from './fixtures/fixtures.command';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    ConsoleModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [BankAccount]
+    }),
+    TypeOrmModule.forFeature([BankAccount])
+  ],
+  controllers: [AppController, BanckAccountController],
+  providers: [AppService, FixturesCommand],
 })
 export class AppModule {}
